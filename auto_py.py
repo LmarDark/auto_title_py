245addbb3a1977c914a022fb328f68a1eb7cc63d
Lucas Matheus <105503258+LmarDark@users.noreply.github.com>
2023-09-21 12:23:19 -0400

    Update auto_py.py

--- auto_py.py
+++ auto_py.py
@@ -2,23 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 
 resposta = requests.get("https://www.google.com/")
+html = resposta.content
 
-print(resposta.text)
+soup = BeautifulSoup(html, "html.parser")
+title = soup.find('title')
 
+print(title)
+print(title.string)
 
-"""
-import requests
-from bs4 import BeautifulSoup
-
-header = {'user-agent': 'Mozilla/5.0'}
-
-resposta = requests.get("https://www.google.com/")
-
-html_doc = resposta.text
-
-soup = BeautifulSoup(html_doc, 'html.parser')
-
-for link in soup.find_all('<title>'):
-    print(link.get('<title>'))
-
-"""
+resposta.text
