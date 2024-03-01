2831f95abee873bfc73e72a3bf2b1cc443ac951c
Lucas Matheus <105503258+LmarDark@users.noreply.github.com>
2024-02-29 20:10:47 -0400

    Update auto_py.py

--- auto_py.py
+++ auto_py.py
@@ -25,6 +25,16 @@ def soupa():
     resposta.text
 
 while True:
-    name()
-    request()
-    soupa()
+    try:
+        name()
+    except:
+        break
+
+    try:
+        request()
+    except:
+        break
+
+    try:
+        soupa()
+    except: break
